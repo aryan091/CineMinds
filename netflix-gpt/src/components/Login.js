c7908f48e5d2c4930b018ac03a25c618aa2ff0a4
Aryan Daftari <aryandaftari7@gmail.com>
2024-08-11 23:46:47 +0530

    Added Form Validation

--- netflix-gpt/src/components/Login.js
+++ netflix-gpt/src/components/Login.js
@@ -1,43 +1,78 @@
-import React , {useState} from 'react'
-import Header from './Header'
-import { BG_URL } from '../utils/constants'
+import React, { useRef, useState } from 'react';
+import Header from './Header';
+import { BG_URL } from '../utils/constants';
+import { checkValidData } from '../utils/validate';
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
-    const [isSignInForm, setIsSignInForm] = useState(true);
+  const email = useRef(null);
+  const password = useRef(null);
+  const name = useRef(null);
 
-    const toggleSignInForm = () => {
-        setIsSignInForm(!isSignInForm);
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
+  const handleButtonClick = () => {
+    let message;
+    if (isSignInForm) {
+      message = checkValidData(null, email.current.value, password.current.value, isSignInForm);
+    } else {
+      message = checkValidData(name.current.value, email.current.value, password.current.value, isSignInForm);
     }
+
+    setErrorMessage(message);
+  };
 
   return (
     <div>
-        <Header/>
-        <div className='absolute'>
-
+      <Header />
+      <div className="absolute">
         <img src={BG_URL} alt="Bg-Image" />
-        </div>
+      </div>
 
-<form action="" className='absolute p-12 w-3/12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-75 rounded-lg'>
+      <form onSubmit={(e) => e.preventDefault()} className="absolute p-12 w-3/12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-75 rounded-lg">
+        <h1 className="font-bold text-3xl py-4">{isSignInForm ? 'Sign In' : 'Sign Up'}</h1>
 
-    <h1 className='font-bold text-3xl py-4'>{ isSignInForm ? "Sign In" : "Sign Up"}</h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            ref={name}
+            className="p-4 my-4 w-full bg-transparent border border-gray rounded-lg"
+          />
+        )}
+        <input
+          type="text"
+          placeholder="Email"
+          ref={email}
+          className="p-4 my-4 w-full bg-transparent border border-gray rounded-lg"
+        />
 
+        <input
+          type="password"
+          placeholder="Password"
+          ref={password}
+          className="p-4 my-4 w-full bg-transparent border border-gray rounded-lg"
+        />
 
-{ !isSignInForm && (<input type="text" placeholder="Full Name" className=' p-4 my-4 w-full bg-transparent border border-gray 
-rounded-lg' />)
-}
-    <input type="text" placeholder="Email" className=' p-4 my-4 w-full bg-transparent border border-gray rounded-lg'  />
+        <p className="text-red-500 text-center font-semibold py-2">{errorMessage}</p>
 
-    <input type="Password" placeholder="Password" className=' p-4 my-4 w-full bg-transparent border border-gray rounded-lg'  />
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg" onClick={handleButtonClick}>
+          {isSignInForm ? 'Sign In' : 'Sign Up'}
+        </button>
 
-    <button className='p-4 my-6 bg-red-700 w-full rounded-lg'>{ isSignInForm ? "Sign In" : "Sign Up"}</button>
-
-    <p className='text-neutral-300 py-4'>{isSignInForm ? "New to Stream AI?" : "Already have an account?"} <span className='text-white font-semibold cursor-pointer' onClick={toggleSignInForm}>{ isSignInForm ? "Sign Up Now" : "Sign In"}.</span></p>
-
-</form>
-
+        <p className="text-neutral-300 py-4">
+          {isSignInForm ? 'New to Stream AI?' : 'Already have an account?'}{' '}
+          <span className="text-white font-semibold cursor-pointer" onClick={toggleSignInForm}>
+            {isSignInForm ? 'Sign Up Now' : 'Sign In'}.
+          </span>
+        </p>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
