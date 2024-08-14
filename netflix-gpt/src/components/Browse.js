6348577f028482f998877ffd35e97b0536d02904
Aryan Daftari <aryandaftari7@gmail.com>
2024-08-14 10:29:36 +0530

    Added Movie Slice for Better Management of Data

--- netflix-gpt/src/components/Browse.js
+++ netflix-gpt/src/components/Browse.js
@@ -1,8 +1,12 @@
 import React, { useEffect } from 'react'
 import Header from './Header'
 import { API_OPTIONS } from '../utils/constants'
+import { addNowPlayingMovies } from '../utils/movieSlice'
+import { useDispatch } from 'react-redux'
 
 const Browse = () => {
+
+  const dispatch = useDispatch()
 
   const getNowPlayingMovies = async () => {
 
@@ -10,7 +14,8 @@ const Browse = () => {
 try {
       const data = await fetch(`https://api.themoviedb.org/3/movie/now_playing?page=1`, API_OPTIONS)
       const response = await data.json()
-      console.log(response)
+      console.log(response.results)
+      dispatch(addNowPlayingMovies(response.results))
 } catch (error) {
   console.log(error)
 }
