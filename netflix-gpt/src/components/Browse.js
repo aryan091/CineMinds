b21e4a1a0df3ee545d5fe010df59d04bf77fe394
Aryan Daftari <aryandaftari7@gmail.com>
2024-08-14 10:19:39 +0530

    Integrated TMDB API for Movies

--- netflix-gpt/src/components/Browse.js
+++ netflix-gpt/src/components/Browse.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
+import { API_OPTIONS } from '../utils/constants'
+
 const Browse = () => {
+
+  const getNowPlayingMovies = async () => {
+
+
+try {
+      const data = await fetch(`https://api.themoviedb.org/3/movie/now_playing?page=1`, API_OPTIONS)
+      const response = await data.json()
+      console.log(response)
+} catch (error) {
+  console.log(error)
+}
+  }
+
+  useEffect(() => {
+    getNowPlayingMovies()
+  }, [])
+
   return (
     <div>
         <Header/>
