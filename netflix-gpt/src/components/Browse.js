70fbc1cf31ecbb55e60a6de2baac6868694ec7fc
Aryan Daftari <aryandaftari7@gmail.com>
2024-08-14 10:37:16 +0530

    Implemented Custom Hook for fetching Movies to make code Cleaner and Readable

--- netflix-gpt/src/components/Browse.js
+++ netflix-gpt/src/components/Browse.js
@@ -1,29 +1,8 @@
-import React, { useEffect } from 'react'
+import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
 import Header from './Header'
-import { API_OPTIONS } from '../utils/constants'
-import { addNowPlayingMovies } from '../utils/movieSlice'
-import { useDispatch } from 'react-redux'
-
 const Browse = () => {
 
-  const dispatch = useDispatch()
-
-  const getNowPlayingMovies = async () => {
-
-
-try {
-      const data = await fetch(`https://api.themoviedb.org/3/movie/now_playing?page=1`, API_OPTIONS)
-      const response = await data.json()
-      console.log(response.results)
-      dispatch(addNowPlayingMovies(response.results))
-} catch (error) {
-  console.log(error)
-}
-  }
-
-  useEffect(() => {
-    getNowPlayingMovies()
-  }, [])
+  useNowPlayingMovies()
 
   return (
     <div>
---
+++ netflix-gpt/src/hooks/useNowPlayingMovies.js
@@ -0,0 +1,27 @@
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/movieSlice";
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
+const useNowPlayingMovies = () => {
+  const dispatch = useDispatch();
+
+  const getNowPlayingMovies = async () => {
+    try {
+      const data = await fetch(
+        `https://api.themoviedb.org/3/movie/now_playing?page=1`,
+        API_OPTIONS
+      );
+      const response = await data.json();
+      console.log(response.results);
+      dispatch(addNowPlayingMovies(response.results));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
+};
+
+export default useNowPlayingMovies;
