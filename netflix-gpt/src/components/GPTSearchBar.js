de6016c7de612f081cee006c5684a17050c143ea
Aryan Daftari <aryandaftari7@gmail.com>
2024-08-15 12:14:55 +0530

    Implemented MultiLanguage Support for GPT Search

--- netflix-gpt/src/components/GPTSearchBar.js
+++ netflix-gpt/src/components/GPTSearchBar.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
+import lang from '../utils/languageConstants';
 
 const GPTSearchBar = () => {
+
+  const langKey = useSelector(store => store.config.lang); 
+    
   return (
     <div className="pt-[35%] md:pt-[10%] flex justify-center">
         <form 
         className="w-full md:w-1/2 bg-black grid grid-cols-12">
-        <input type="text" placeholder='What would you like to Search Today..?' className='p-4 m-4 col-span-9 '/>
-            <button className=' text-white bg-transparent border border-white rounded-md font-semibold col-span-3 m-4 py-2 px-4'>Search</button>
+        <input type="text" placeholder={lang[langKey].gptSearchPlaceholder}className='p-4 m-4 col-span-9 '/>
+            <button className=' text-white bg-transparent border border-white rounded-md font-semibold col-span-3 m-4 py-2 px-4'>{lang[langKey].search}</button>
         </form>
     </div>
   )
--- netflix-gpt/src/components/Header.js
+++ netflix-gpt/src/components/Header.js
@@ -6,10 +6,12 @@ import { auth } from '../utils/firebase';
 import {  useDispatch, useSelector } from 'react-redux';
 import { addUser, removeUser } from '../utils/userSlice';
 import {toggleGptSearchView} from '../utils/gptSlice'
-
+import { SUPPORTED_LANGUAGES } from '../utils/constants';
+import {changeLanguage} from '../utils/configSlice'
 const Header = () => {
 
     const user = useSelector(store => store.user);
+    const gptView = useSelector(store => store.gpt.showGptSearch);
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
@@ -40,7 +42,7 @@ const Header = () => {
             }
           });
 
-          // Unsubscribe from auth state changea when component unmounts
+          // Unsubscribe from auth state changes when component unmounts
           return () => {
             unsubscribe();
           }
@@ -51,6 +53,10 @@ const Header = () => {
       dispatch(toggleGptSearchView())
     }
 
+    const handleLanguageChange = (event) => {
+      dispatch(changeLanguage(event.target.value))
+    }
+
   return (
     <div className='header w-screen absolute px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between items-center'>
         <img src={LOGO} alt="Logo" className='logo w-44'/>
@@ -59,6 +65,14 @@ const Header = () => {
           
           
           <div  className='flex gap-4'>
+
+{        gptView &&    (<select className='p-2 m-2 bg-slate-900 text-white font-semibold' onClick={handleLanguageChange}>
+{
+  SUPPORTED_LANGUAGES.map((language) => (
+    <option key={language.identifier} value={language.identifier}>{language.name}</option>
+  ))
+}
+            </select>)}
 
             <button type="button" className='py-2 px-4 text-white bg-transparent border border-white rounded-md font-semibold mx-4' onClick={handleGPTSearchClick}>
               GPT Search
